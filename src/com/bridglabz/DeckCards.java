caf496c756c61e21cfd6bc8fba545b081413de8b
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-05-05 19:57:26 +0530

    Added: Welcome to Objected Oriented Programming Language.

---
+++ src/com/bridglabz/DeckCards.java
@@ -0,0 +1,8 @@
+package com.bridglabz;
+
+public class DeckCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Object Oriented Programming Problems.");
+    }
+}
+
